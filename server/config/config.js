7542adcf1ec1716dc466f87ede03c36dfed9d5c0
yandre13.elsf <yandre13.elsf@gmail.com>
2018-10-11 23:56:55 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:123456a@ds221271.mlab.com:21271/cafe'
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB
